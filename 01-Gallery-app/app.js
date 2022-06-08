dd786d4cd03f6f774417d6c96ef531c769f65ed2
Yaroslav <crosskyiv@gmail.com>
2022-06-08 17:08:36 +0300

    modal complete

--- 01-Gallery-app/app.js
+++ 01-Gallery-app/app.js
@@ -11,36 +11,49 @@ function getElement(selection){
 function Gallery(element){
 
 	this.conatiner = element
-
-	this.list = [...element.querySelectorAll('.img')]  
+	// list -> images-category
+	this.list = [...element.querySelectorAll('.img')] 
 	// target
 	this.modal = getElement('.modal')
-	this.moddalImg = getElement('.img')
+	this.modalImg = getElement('.main-img')
+	this.imageName = getElement('.image-name')
 	this.modalImages = getElement('.modal-images')
-	this.closeBtn = getElement('.close-btn')
-	this.nextBtn = getElement('.next-btn')
-	this.prevBtn = getElement('.prev-btn')
-// bind functions
+	//this.closeBtn = getElement('.close-btn')
+	//this.nextBtn = getElement('.next-btn')
+	//this.prevBtn = getElement('.prev-btn')
 
+	// bind functions
 	this.openModal = this.openModal.bind(this) 
+	// container event
 	this.conatiner.addEventListener('click', 
 		function(e){ 
-			console.log(this);
-			this.openModal()
+			if(e.target.classList.contains('img')){
+				this.openModal(e.target, this.list)
+			}
+
 		}.bind(this)) 
-		
+	
+}
+// окремо обране зображення з методом openModel (e.target,this.list - обрати об'єкт з націленої дії; this.list з певного переліку- категорії)
+Gallery.prototype.openModal = function(selectedImage, list){
+	this.setMainImage(selectedImage)
+
+	// ітеруємо list і на кожній ітерації ми маємо доступ до image- аргумента
+	this.modalImages.innerHTML = list.map(function(image){
+		return `<img src='${image.src}' title='${image.title}' data-id='${image.dataset.id}' class='${selectedImage.dataset.id === image.dataset.id ? "modal-img selected" : "modal-img" }' />`
+	}).join('')
+
+	this.modalImages.classList.add('selected')
+	this.modal.classList.add('open')
 }
 
-
-Gallery.prototype.openModal = function(){
-	console.log('open modal');
-	this.modal.classList.add('open')
+Gallery.prototype.setMainImage = function(selectedImage){
+	this.modalImg.src = selectedImage.src
+	this.imageName.textContent = selectedImage.title
 }
 
 const nature= new Gallery(getElement('.nature'))
 const city= new Gallery(getElement('.city'))
-
-
 
 
 
